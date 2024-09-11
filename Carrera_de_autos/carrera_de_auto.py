68493c62b27851e5e4d67ae5a2f23211b1a6bccb
Alexis Artaza <alexisangelartaza@gmail.com>
2024-09-10 21:11:55 -0300

    kk

--- Carrera_de_autos/carrera_de_auto.py
+++ Carrera_de_autos/carrera_de_auto.py
@@ -5,14 +5,16 @@ import random
 # Ventana principal
 ventana_principal = tk.Tk()
 ventana_principal.title('Carrera de autos')
-ventana_principal.geometry('1800x800+60+108')
+ventana_principal.geometry('1600x800+60+0')
 ventana_principal.config(background='white')
 
 class auto_de_carrera:
     def __init__(self, nombre, nro):
         self.name = nombre
+        self.id = nro
+        self.vuelta = 0
         self.auto_url = tk.PhotoImage(file=f"Carrera_de_autos/images/auto{nro}.png").zoom(4).subsample(50)
-        self.pista = tk.Canvas(label_frame, width=1800, height=100, bg='grey')
+        self.pista = tk.Canvas(label_frame, width=1600, height=100, bg='grey')
         self.posicion_x = 0
         self.auto_imagen = self.pista.create_image(self.posicion_x, 50, anchor="nw", image=self.auto_url)
         self.pista.pack(pady=5)
@@ -27,20 +29,13 @@ auto3 = auto_de_carrera('Rojo',3)
 auto4 = auto_de_carrera('Verde',4)
 competidores = [auto1, auto2, auto3, auto4]
 
-def verificar_ganador(competidores):
-    for competidor in competidores:
-        if competidor.posicion_x >= 1700:
-            print(f"Ganador: {competidor.name}")
-            return competidor.name
-    return None
-
 def reiniciar():
     global competidores
+    global label_ganador
     for competidor in competidores:
         competidor.posicion_x = 0
         competidor.pista.coords(competidor.auto_imagen, competidor.posicion_x, 50)
     ventana_principal.update()
-
     empezar_juego()
 
 def empezar_juego():
@@ -54,19 +49,39 @@ def empezar_juego():
         time.sleep(0.05)
         for competidor in competidores:
             nueva_posicion = random.randint(0, 10)
+            if nueva_posicion == 10:
+                nueva_posicion_boost = random.randint(0,3)
+                nueva_posicion += nueva_posicion_boost
+            if nueva_posicion == 0:
+                nueva_posicion_boost = random.randint(1,3)
+                nueva_posicion += nueva_posicion_boost
             competidor.pista.move(competidor.auto_imagen, nueva_posicion, 0)
             competidor.posicion_x += nueva_posicion
-            print(competidor.posicion_x)
-            
-        ganador = verificar_ganador(competidores)
+            print(f"{competidor.name}: {competidor.posicion_x}")
+           
+       
+        def verificar_vuelta(competidores):
+            for competidor in competidores:
+                if competidor.vuelta == 3:
+                    return competidor.name
+                if competidor.posicion_x >= 1500:
+                    competidor.vuelta += 1
+                    competidor.posicion_x = 0+(competidor.posicion_x-1500)
+                    competidor.pista.coords(competidor.auto_imagen, 
+                    competidor.posicion_x, 50)
+                    b1.config(text=f'Vuelta{competidor.vuelta}')
+                
+                
+        #vueltas_completadas = verificar_vuelta()
+        ganador = verificar_vuelta(competidores)
 
         if ganador:
-            label_ganador = tk.Label(ventana_principal, text=f"{ganador} ganó!!", font=('Arial', 20))
+            label_ganador = tk.Label(ventana_principal, text=f"{ganador} ganó!!", font=('Arial', 20), bg="white")
             label_ganador.pack(pady=5)
             b1.config(text='Volver a jugar', command=reiniciar)
 
 
-l2 = tk.Label(ventana_principal, text='Presiona jugar para empezar la carrera', font=('Arial', 20), bg='white')
+l2 = tk.Label(ventana_principal, text='Para ganar el auto debe llegar 3 veces al final. El auto avanzara del 0 a 10 aleatoriamente cada 0,05segundo', font=('Arial', 20), bg='white')
 l2.pack(pady=25)
 
 b1 = tk.Button(ventana_principal, text='JUGAR!', font=('Arial', 20), height=2, width=15, command=empezar_juego)
